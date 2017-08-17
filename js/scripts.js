3ce8b1b4bbda5a45eec17ddcbbe0b1481a99af5f
Dan Nollette and snow vilay <snow@>
2017-08-17 09:13:03 -0700

    Add a functiont to make sure user input is a number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 //Business Logic(Back-End)
-
+function converter(number){
+  if (number !== number) {
+    return "That's not a valid input. Please enter a whole number, less than 4,000."
+  };
+};
 
 
 
@@ -15,6 +19,13 @@
 
 
 //User Interface (UI or Front-End)
-$(document).ready(function(){
 
+$(document).ready(function(){
+  $("#roman-numeral-form").submit(function(event){
+    var number = parseInt($("input#num-input").val());
+    alert(number);
+    var output = converter(number);
+    $("#output").text(output);
+    event.preventDefault();
+  });
 });
